2f6d2d62747f82c1eb66f827b3dc054847cb2c23
Vicente Barros <vmabarros@ua.pt>
2021-06-19 00:51:31 +0100

    STs de Programas e episodios

---
+++ database/sql-p/st-detailsPrograma.sql
@@ -0,0 +1,23 @@
+-- IF Object_Id('dbo.detailsPrograma', 'P') IS NOT NULL DROP PROCEDURE dbo.detailsPrograma
+
+--? Será que vale a pena fazer um inner join com estação sabendo que é a mesma coisa? 
+CREATE PROC dbo.detailsPrograma
+    @id as int
+AS
+    SELECT 
+       p.nome,p.descricao,c.designacao as classificacao,p.foto,u.nome as estacao, AVG(ep.duracao) as duracao, COUNT(ep.id_programa) as numero_episodios
+    FROM programa p
+        INNER JOIN classificacao c
+            ON p.classificacao = c.id
+        INNER JOIN estacao e
+            ON p.estacao = e.estacaoid
+        INNER JOIN [user] u
+            ON e.estacaoid = u.userid
+        INNER JOIN episodio ep
+            ON ep.id_programa = p.id
+    WHERE p.id = @id
+    GROUP BY p.nome,p.descricao,c.designacao,p.foto,u.nome
+
+
+EXEC dbo.detailsPrograma 1 
+
---
+++ database/sql-p/st-listEpisodios.sql
@@ -0,0 +1,12 @@
+-- IF Object_Id('dbo.listEpisodios', 'P') IS NOT NULL DROP PROCEDURE dbo.listEpisodios
+CREATE PROC dbo.listEpisodios @id AS INT, @PageNumber AS INT = 1,@RowsPerPage AS INT = 15
+AS 
+    SELECT 
+        e.titulo, e.n_episodio,e.data_ini,e.data_fim
+    FROM episodio e
+    WHERE e.id_programa = @id
+    ORDER BY e.n_episodio
+    OFFSET (@PageNumber-1)*@RowsPerPage ROWS
+    FETCH NEXT @RowsPerPage ROWS ONLY
+
+EXEC dbo.listEpisodios 3
