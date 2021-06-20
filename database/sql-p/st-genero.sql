b6483a38853e7636e0a9884dc54ce37311345002
Vicente Barros <vmabarros@ua.pt>
2021-06-20 01:50:45 +0100

    Fix: st-genero só com géneros com pelo menos 1 programa

--- database/sql-p/st-genero.sql
+++ database/sql-p/st-genero.sql
@@ -1,12 +1,16 @@
 -- IF Object_Id('dbo.generoList', 'P') IS NOT NULL DROP PROCEDURE dbo.generoList
 
 -- frontend implemented
-CREATE PROCEDURE dbo.generoList @PageNumber AS INT = 1,@RowsPerPage AS INT = 15
+CREATE PROCEDURE dbo.generoList @PageNumber AS INT = 1,@RowsPerPage AS INT = 16
 AS
     SELECT 
-        designacao, imagem
-            FROM dbo.categoria
-    ORDER BY popularidade DESC
+        c.designacao, c.imagem
+    FROM dbo.categoria c
+        RIGHT JOIN dbo.programa_categoria pc
+            ON c.id = pc.categoria
+        GROUP BY c.designacao, c.imagem,popularidade
+        ORDER BY popularidade DESC
+
     OFFSET (@PageNumber-1)*@RowsPerPage ROWS
     FETCH NEXT @RowsPerPage ROWS ONLY
 
@@ -14,7 +18,7 @@ AS
 
 /*
 Devolve apenas os top 15 da primeira página
-EXEC GeneroListb 
+EXEC GeneroList 1
 
 Devolve os top A da página B
 EXEC GeneroList A, B
