1a689f7f578ce09f2a6126a9ad3a06e46f5319dd
André Morais <48691769+moraisandreua@users.noreply.github.com>
2021-06-24 00:13:47 +0100

    notes?

--- database/sql-p/udf-search.sql
+++ database/sql-p/udf-search.sql
@@ -1,18 +1,18 @@
 --DROP FUNCTION dbo.doSearch;
 
-CREATE FUNCTION dbo.doSearch (@searchQuery AS VARCHAR(100) = '', @resultNumber AS INT) RETURNS @datatable TABLE (id INT ,nome VARCHAR(100), datatype VARCHAR(100))
+CREATE FUNCTION [dbo].[doSearch] (@searchQuery AS VARCHAR(100) = '', @resultNumber AS INT) RETURNS @datatable TABLE (id INT ,nome VARCHAR(100), datatype VARCHAR(100), foto VARCHAR(256))
 AS
     BEGIN
         IF @searchQuery <> '' 
             BEGIN
                 --Procurar por programas 
-                INSERT @datatable SELECT TOP(@resultNumber) id,nome, 'programa' FROM programa WHERE nome LIKE '%'+@searchQuery+'%'
+                INSERT @datatable SELECT TOP(@resultNumber) id,nome,'programa',foto FROM programa WHERE nome LIKE '%'+@searchQuery+'%'
                 --Procurar por playlist
-                INSERT @datatable SELECT TOP (@resultNumber) id,designacao,'playlist' FROM listareproducao WHERE designacao LIKE '%'+@searchQuery+'%'
+                INSERT @datatable SELECT TOP (@resultNumber) id,designacao,'playlist',foto FROM listareproducao WHERE designacao LIKE '%'+@searchQuery+'%'
                 --Procurar por user
-                INSERT @datatable SELECT TOP (@resultNumber) userid,username,'user' FROM [user] WHERE username LIKE '%'+@searchQuery+'%'
+                INSERT @datatable SELECT TOP (@resultNumber) userid,username,'user',foto FROM [user] WHERE username LIKE '%'+@searchQuery+'%'
                 --Procurar por Géneros
-                INSERT @datatable SELECT TOP (@resultNumber) id,designacao,'genero' FROM categoria WHERE designacao LIKE '%'+@searchQuery+'%' ORDER BY popularidade
+                INSERT @datatable SELECT TOP (@resultNumber) id,designacao,'genero',imagem FROM categoria WHERE designacao LIKE '%'+@searchQuery+'%' ORDER BY popularidade
             END
         
         RETURN
