2de4d28b61c89416dc41b1213ba241fb5af6304c
Vicente Barros <vmabarros@ua.pt>
2021-06-24 11:56:21 +0100

    Fix: CreatePrograma

--- database/sql-p/st-createPrograma.sql
+++ database/sql-p/st-createPrograma.sql
@@ -1,23 +1,29 @@
---IF Object_Id('dbo.seleccreateProgramatedEstacao', 'P') IS NOT NULL DROP PROCEDURE dbo.createPrograma
+--IF Object_Id('dbo.createPrograma', 'P') IS NOT NULL DROP PROCEDURE dbo.createPrograma
 CREATE PROC dbo.createPrograma
     @userid AS INT, @nome AS VARCHAR(50),@descricao AS VARCHAR(280), @classificacao AS VARCHAR(100), @foto AS VARCHAR(256)
 AS
 BEGIN
-    IF EXISTS(SELECT estacaoid FROM estacao WHERE estacaoid = @userid)
+    DECLARE @classificacaoid as INT
+    SET @classificacaoid = (SELECT id FROM classificacao WHERE classificacao.designacao = TRIM(@classificacao))
+
+
+    IF EXISTS(SELECT estacaoid FROM estacao WHERE estacaoid = @userid) AND NOT EXISTS (SELECT id from programa WHERE programa.nome = @nome)
         BEGIN
             BEGIN TRY
                 BEGIN TRAN
-                    INSERT INTO programa VALUES (@nome,@descricao,(SELECT id FROM classificacao WHERE classificacao.designacao = @descricao),@userid,@foto)
+                    INSERT INTO programa([nome],[descricao],[classificacao],[estacao],[foto]) VALUES (@nome,@descricao,@classificacaoid,@userid,@foto)
                     COMMIT TRAN
             END TRY
             BEGIN CATCH
                 PRINT @@ERROR
-                    ROLLBACK TRANSACTION T1
+                    ROLLBACK TRAN
                     RETURN 0
             END CATCH 
         END 
     ELSE
         BEGIN
-            RETURN 0
-        END 
-END 
+            PRINT 0
+        END
+END
+
+EXEC dbo.createPrograma 60,'Temp','Olá mundo','jovem','http://www.google.com'
